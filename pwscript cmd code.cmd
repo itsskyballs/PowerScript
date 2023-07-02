4f01fc49875fa666f8b1e06da1174a38043a2501
macaroneeeyosumc <111575685+stevenkiethanete@users.noreply.github.com>
2023-07-02 18:05:12 +0800

    No longer Batchfile

--- pwscript cmd code.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-title Read this first&echo PowerScript is a Simple vbs file that lets you boot into any Special Windows options or anything&echo.&echo You don't have to worry about Pressing keys,Very Frustrating right?&echo.&echo Going to BIOS by Pressing F2 key twice or more(depends on your board)?,No problem,Go to Special Options and click PowerScript UEFI,Click Yes and it will boot into BIOS Without pressing F2 key&echo.&echo PowerScript's command is powered by Command Prompt with shutdown executable&echo.&echo If you don't understand these grammars,please use Grammarly&echo.&echo If you get this message "You're About to be Signed Out",Don't mind that because it doesn't do anything&echo.&echo *****************************************************************&echo.&echo           PowerScript is Created by Steven Kieth&echo.&echo *****************************************************************&echo.&echo Thats All.
-:halt
-pause
-## this is cmd code so you can copy/paste it if you want to ##
